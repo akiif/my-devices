ce78b3c8a1de8c48a54c572da9fe9819f79c67ec
Akif Mohammed <akiif.dev@gmail.com>
2026-03-27 20:38:19 +0300

    refactor(ipad): Removed the ipad-air from the tablets list.

--- src/data/tablets.js
+++ src/data/tablets.js
@@ -1,6 +1,5 @@
 // import images
 import SamsungGalaxyTab2 from "../assets/images/tablets/samsung-galaxy-tab-2.webp";
-import IpadAir5 from "../assets/images/tablets/ipad-air-5.webp";
 
 export default [
   {
@@ -9,14 +8,6 @@ export default [
     date: "01/07/2013",
     year: 2013,
     link: "https://www.gsmarena.com/samsung_galaxy_tab_2_7_0_p3100-4543.php",
-    img: SamsungGalaxyTab2
+    img: SamsungGalaxyTab2,
   },
-  {
-    id: 2,
-    name: "Apple iPad Air 5 (2022)",
-    date: "10/02/2023",
-    year: 2023,
-    link: "https://www.gsmarena.com/apple_ipad_air_(2022)-11411.php",
-    img: IpadAir5
-  }
-]
+];
